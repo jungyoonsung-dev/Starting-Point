0f1cae8b5bd2f2bb34215cb99bf4fea59b178eb6
jungyoonsung-dev <jungyoonsung.dev@gmail.com>
2020-01-07 00:01:10 +0900

    2019 01 07  00/01

--- app/src/main/java/com/jungyoonsung/startingpoint/Tab/TabSchool.java
+++ app/src/main/java/com/jungyoonsung/startingpoint/Tab/TabSchool.java
@@ -2,25 +2,33 @@ package com.jungyoonsung.startingpoint.Tab;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 import com.jungyoonsung.startingpoint.R;
 
 public class TabSchool extends Fragment {
 
     Context thisContext;
 
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+
     @Override
     public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tabschool, container, false);
-
-
         thisContext = container.getContext();
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
 
 
         return view;

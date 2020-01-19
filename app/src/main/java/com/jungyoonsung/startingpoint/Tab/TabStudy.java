ae8afcdb6843b8fe0b1c11787536014c0415ea93
jungyoonsung-dev <jungyoonsung.dev@gmail.com>
2020-01-19 11:32:53 +0900

    2020 01 19  11/32

--- app/src/main/java/com/jungyoonsung/startingpoint/Tab/TabStudy.java
+++ app/src/main/java/com/jungyoonsung/startingpoint/Tab/TabStudy.java
@@ -1,25 +1,13 @@
 package com.jungyoonsung.startingpoint.Tab;
 
 import android.content.Context;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
-
-import com.applandeo.materialcalendarview.CalendarView;
-import com.applandeo.materialcalendarview.EventDay;
-import com.applandeo.materialcalendarview.utils.DateUtils;
-import com.google.android.material.drawable.DrawableUtils;
 import com.jungyoonsung.startingpoint.R;
-
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
 
 public class TabStudy extends Fragment {
 
@@ -31,65 +19,7 @@ public class TabStudy extends Fragment {
         View view = inflater.inflate(R.layout.tabstudy, container, false);
         thisContext = container.getContext();
 
-        List<EventDay> events = new ArrayList<>();
-
-//        Calendar calendar = Calendar.getInstance();
-//        events.add(new EventDay(calendar, DrawableUtils.getCircleDrawableWithText(this, "M")));
-
-        Calendar calendar1 = Calendar.getInstance();
-        calendar1.add(Calendar.DAY_OF_MONTH, 10);
-        events.add(new EventDay(calendar1, R.drawable.ic_facebook));
-
-        Calendar calendar2 = Calendar.getInstance();
-        calendar2.add(Calendar.DAY_OF_MONTH, 10);
-        events.add(new EventDay(calendar2, R.drawable.ic_instagram, Color.parseColor("#228B22")));
-
-        Calendar calendar3 = Calendar.getInstance();
-        calendar3.add(Calendar.DAY_OF_MONTH, 7);
-        events.add(new EventDay(calendar3, R.drawable.ic_github));
-//
-//        Calendar calendar4 = Calendar.getInstance();
-//        calendar4.add(Calendar.DAY_OF_MONTH, 13);
-//        events.add(new EventDay(calendar4, DrawableUtils.getThreeDots(thisContext)));
-
-        CalendarView calendarView = (CalendarView) view.findViewById(R.id.calendarView);
-
-        Calendar min = Calendar.getInstance();
-        min.add(Calendar.MONTH, -2);
-
-        Calendar max = Calendar.getInstance();
-        max.add(Calendar.MONTH, 2);
-
-        calendarView.setMinimumDate(min);
-        calendarView.setMaximumDate(max);
-
-        calendarView.setEvents(events);
-
-        calendarView.setDisabledDays(getDisabledDays());
-
-        calendarView.setOnDayClickListener(eventDay ->
-                Toast.makeText(thisContext.getApplicationContext(),
-                        eventDay.getCalendar().getTime().toString() + " "
-                                + eventDay.isEnabled(),
-                        Toast.LENGTH_SHORT).show());
 
         return view;
-    }
-
-    private List<Calendar> getDisabledDays() {
-        Calendar firstDisabled = DateUtils.getCalendar();
-        firstDisabled.add(Calendar.DAY_OF_MONTH, 2);
-
-        Calendar secondDisabled = DateUtils.getCalendar();
-        secondDisabled.add(Calendar.DAY_OF_MONTH, 1);
-
-        Calendar thirdDisabled = DateUtils.getCalendar();
-        thirdDisabled.add(Calendar.DAY_OF_MONTH, 18);
-
-        List<Calendar> calendars = new ArrayList<>();
-        calendars.add(firstDisabled);
-        calendars.add(secondDisabled);
-        calendars.add(thirdDisabled);
-        return calendars;
     }
 }

271ffe9e9b9d749924e32c55582bc2f58ce8ced1
jungyoonsung-dev <jungyoonsung.dev@gmail.com>
2020-01-24 09:36:15 +0900

    2020 01 24  09/36

--- app/src/main/java/com/jungyoonsung/startingpoint/Tab/TabStudy.java
+++ app/src/main/java/com/jungyoonsung/startingpoint/Tab/TabStudy.java
@@ -1,13 +1,24 @@
 package com.jungyoonsung.startingpoint.Tab;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.github.sundeepk.compactcalendarview.domain.Event;
 import com.jungyoonsung.startingpoint.R;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import static com.firebase.ui.auth.AuthUI.TAG;
 
 public class TabStudy extends Fragment {
 
@@ -19,6 +30,43 @@ public class TabStudy extends Fragment {
         View view = inflater.inflate(R.layout.tabstudy, container, false);
         thisContext = container.getContext();
 
+        final CompactCalendarView compactCalendarView = (CompactCalendarView) view.findViewById(R.id.compactcalendar_view);
+        // Set first day of week to Monday, defaults to Monday so calling setFirstDayOfWeek is not necessary
+        // Use constants provided by Java Calendar class
+        compactCalendarView.setFirstDayOfWeek(Calendar.MONDAY);
+
+        // Add event 1 on Sun, 07 Jun 2015 18:20:51 GMT
+        Event ev1 = new Event(Color.GREEN, 1433701251000L, "Some extra data that I want to store.");
+        compactCalendarView.addEvent(ev1);
+
+        // Added event 2 GMT: Sun, 07 Jun 2015 19:10:51 GMT
+        Event ev2 = new Event(Color.GREEN, 1433704251000L);
+        compactCalendarView.addEvent(ev2);
+
+        Event ev3 = new Event(Color.GREEN, 1579825932L, "Some extra data that I want to store.");
+        compactCalendarView.addEvent(ev3);
+
+        // Query for events on Sun, 07 Jun 2015 GMT.
+        // Time is not relevant when querying for events, since events are returned by day.
+        // So you can pass in any arbitary DateTime and you will receive all events for that day.
+        List<Event> events = compactCalendarView.getEvents(1433701251000L); // can also take a Date object
+
+        // events has size 2 with the 2 events inserted previously
+        Log.d("TEST", "Events: " + events);
+
+        // define a listener to receive callbacks when certain events happen.
+        compactCalendarView.setListener(new CompactCalendarView.CompactCalendarViewListener() {
+            @Override
+            public void onDayClick(Date dateClicked) {
+                List<Event> events = compactCalendarView.getEvents(dateClicked);
+                Log.d("TEST", "Day was clicked: " + dateClicked + " with events " + events);
+            }
+
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
+                Log.d("TEST", "Month was scrolled to: " + firstDayOfNewMonth);
+            }
+        });
 
         return view;
     }
